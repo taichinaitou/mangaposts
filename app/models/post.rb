1fe9e7b270fa2f0c0661118a40ee5c34e0afa77c
taichi.naitou <taisan1313@ezweb.ne.jp>
2021-08-13 08:23:05 +0000

    validates content

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   
   validates :title, presence: true, length: { maximum: 50 }
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 500 }
 end
